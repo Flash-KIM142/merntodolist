d5a25b7d53280502a76c8411378407ba7cd238e0
Flash KIM <62537294+Flash-KIM142@users.noreply.github.com>
2020-09-03 16:22:58 +0900

    create-todo comp completed

---
+++ src/components/todos-list.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default  class TodosList extends Component {
+    render() {
+        return (
+            <div>
+                <h3 style={{marginTop:20}}>Welcome to Todos List Component!</h3>
+            </div>
+        )
+    }
+}
